637d4f265a5d264617e62a71293e70ebf99eedf1
smotti <wottis@gmail.com>
2026-01-07 22:08:35 +0800

    Add comments to utils module

--- Scripts/utils.lua
+++ Scripts/utils.lua
@@ -8,10 +8,13 @@ local function Log(...)
 
     if n == 0 then
         return
+    -- Only `message` was provided
     elseif n == 1 then
         print(string.format("[TFWWorkbench] %s\n", args[1]))
+    -- `message` and `component`
     elseif n == 2 then
         print(string.format("[TFWWorkbench:%s] %s\n", args[2], args[1]))
+    -- `message`, `component` and `function`
     elseif n == 3 then
         print(string.format("[TFWWorkbench:%s:%s] %s\n", args[2], args[3], args[1]))
     end
